8abe90bf46edd46a37cb5d00d5293f2a0473ed70
SethDocherty <sddocherty@gmail.com>
2026-01-22 14:11:09 -0500

    Removing work as considerable modifications have been made to the design spec.
    Planning to start fresh with new agent context

--- tests/test_exceptions.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from stac_manager.exceptions import StacManagerError, ConfigurationError
-
-def test_exception_inheritance():
-    err = ConfigurationError("Bad config")
-    assert isinstance(err, StacManagerError)
-    assert str(err) == "Bad config"
--- tests/test_registry.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from stac_manager.registry import get_module_class
-from stac_manager.exceptions import WorkflowConfigError
-
-def test_get_unknown_module():
-    with pytest.raises(WorkflowConfigError):
-        get_module_class("UnknownModule")
